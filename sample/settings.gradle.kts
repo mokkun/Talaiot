f5a39e8ea7d58d9fa896a2dd1281e9627ea05ba1
Mozart Petter <mozart@petter.io>
2020-11-03 12:08:17 +0900

    Update sample to use new base plugin

--- sample/settings.gradle.kts
+++ sample/settings.gradle.kts
@@ -5,8 +5,8 @@ pluginManagement {
     }
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.id == "com.cdsap.talaiot") {
-                useModule("com.cdsap:talaiot:${requested.version}")
+            if (requested.id.id == "com.cdsap.talaiot.plugin.base") {
+                useModule("com.cdsap.talaiot.plugin:base:${requested.version}")
             }
         }
     }
