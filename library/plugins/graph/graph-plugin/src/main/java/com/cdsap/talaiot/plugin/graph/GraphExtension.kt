6541a7ae25e19bcb3cbb01385ed33f366986824f
Mozart Petter <mozart@petter.io>
2020-11-03 14:29:57 +0900

    Update GraphExtension and add some documentation

--- library/plugins/graph/graph-plugin/src/main/java/com/cdsap/talaiot/plugin/graph/GraphExtension.kt
+++ library/plugins/graph/graph-plugin/src/main/java/com/cdsap/talaiot/plugin/graph/GraphExtension.kt
@@ -1,20 +1,34 @@
 package com.cdsap.talaiot.plugin.graph
 
 import com.cdsap.talaiot.TalaiotExtension
-import com.cdsap.talaiot.publisher.PublishersConfiguration
 import groovy.lang.Closure
 import org.gradle.api.Project
 
-class GraphExtension(project: Project) : TalaiotExtension(project) {
-    /**
-     * General Publisher configuration included in the build.
-     */
+/**
+ * A [TalaiotExtension] for the Graph plugin.
+ *
+ * Note: extensions classes need to be declared as `open` so the Gradle internal factory can
+ * dynamically create instances of them.
+ *
+ * @param project The Gradle [Project] that contains this extension.
+ */
+open class GraphExtension(project: Project) : TalaiotExtension(project) {
     internal var publishers: GraphConfiguration? = null
 
-    fun publishers(block: PublishersConfiguration.() -> Unit) {
+    /**
+     * [GraphConfiguration] accessor for the [GraphExtension].
+     *
+     * @param block The configuration block for the [GraphExtension] publisher.
+     */
+    fun publishers(block: GraphConfiguration.() -> Unit) {
         publishers = GraphConfiguration(project).also(block)
     }
 
+    /**
+     * [GraphConfiguration] accessor for the [GraphExtension].
+     *
+     * @param closure The configuration closure for the [GraphExtension] publisher.
+     */
     fun publishers(closure: Closure<*>) {
         publishers = GraphConfiguration(project)
         closure.delegate = publishers
